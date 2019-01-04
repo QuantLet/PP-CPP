c77f1ccd5219136fb830b4003dd00069f0affa36
Adrian Ji <jixiaohao_adr@hotmail.com>
2019-01-04 17:38:47 +0100

    Code Style

--- PP_CPP_Simulation.R
+++ PP_CPP_Simulation.R
@@ -13,18 +13,23 @@ lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
 # Poisson Generator
 PPgen=function(lambda){
-  X=0; Sum=0; flag=0
+  X=0
+  Sum=0
+  flag=0
+
   while (flag==0){
     E=-log(runif(1)); Sum = Sum + E
-    if (Sum < lambda) { X=X+1} else { flag=1}
+    if (Sum < lambda) {X=X+1} else {flag=1}
   }
   return(X)
-  
 }
 
 # Poisson Process
 PP=function(lambda, N, T){
-  h=T/N; t=(0:T)/N; X=rep(0, N+1); I=rep(0,N); X[1]=0
+  h=T/N; t=(0:T)/N 
+  X=rep(0, N+1)
+  I=rep(0,N)
+  X[1]=0
   for(i in 1:N) {
     I[i]=PPgen(h*lambda); X[i+1]=X[i] + I[i]}
   return(X)
@@ -32,7 +37,12 @@ PP=function(lambda, N, T){
 
 #Compound Poisson Process
 CPP=function(lambda, T, N) {
-  h=T/N; t=(0:T)/N; X=rep(0, N+1); F=rep(0, N+1); I=rep(0,N); X[1]=0
+  h=T/N
+  t=(0:T)/N
+  X=rep(0, N+1)
+  F=rep(0, N+1)
+  I=rep(0,N)
+  X[1]=0
   for(i in 1:N) {
     I[i]=PPgen(h*lambda)
     if (I[i]==0){F[i]=0} else {F[i]=rnorm(1)}
@@ -40,19 +50,25 @@ CPP=function(lambda, T, N) {
   return(X)
 }
 
-# Generate 10 Poisson Process Paths and 10 Compound Poisson Process Paths
-lambda = 100; N = 1000; T = 1; stim_up = 10; col_name = c('S1', 'S2','S3', 'S4','S5', 'S6','S7', 'S8','S9', 'S10', 'time')
-
 # Initialization
-S_PP = matrix(0, stim_up, N+1); S_CPP = matrix(0, stim_up, N+1)
+lambda = 100
+N = 1000; T = 1
+stim_up = 10
+col_name = c('S1', 'S2','S3', 'S4','S5', 'S6','S7', 'S8','S9', 'S10', 'time')
+S_PP = matrix(0, stim_up, N+1)
+S_CPP = matrix(0, stim_up, N+1)
 
 # Generating 
 for(i in 1:stim_up){
-  S_CPP[i,] = CPP(lambda, T, N); S_PP[i,] = PP(lambda, N, T)
+  S_CPP[i,] = CPP(lambda, T, N)
+  S_PP[i,] = PP(lambda, N, T)
 }
-S_PP = data.frame(t(S_PP)); S_CPP = data.frame(t(S_CPP))
-S_PP$time = c(0:1000); S_CPP$time = c(0:1000)
-colnames(S_PP) = col_name; colnames(S_CPP) = col_name
+S_PP = data.frame(t(S_PP))
+S_CPP = data.frame(t(S_CPP))
+S_PP$time = c(0:1000)
+S_CPP$time = c(0:1000)
+colnames(S_PP) = col_name
+colnames(S_CPP) = col_name
 
 # Plotting
 pic_1 = xyplot(S1 + S2 + S3 + S4 + S5 + S6 + S7 + S8 + S9 + S10 ~ time, data = S_PP, 
@@ -73,5 +89,5 @@ pic_2 = xyplot(S1 + S2 + S3 + S4 + S5 + S6 + S7 + S8 + S9 + S10 ~ time, data = S
 
 print(pic_1, position=c(0, .5, 1, 1), more=TRUE)
 print(pic_2, position=c(0, 0, 1, .5))
-dev.copy(png,'PP-CPP.png')
-dev.off()
+# dev.copy(png,'PP-CPP.png')
+# dev.off()
